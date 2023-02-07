be6b7321415b84c46600d51a41aff9c6536658a7
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-07 17:17:59 +0100

    started the userController

--- server/controller/UserController.ts
+++ server/controller/UserController.ts
@@ -1,5 +1,21 @@
 import { Request,Response,NextFunction } from "express";
 import UserModel from "../model/UserModel";
+import { asyncHandler } from "../utils/asyncHandler";
+import { AppError,HttpCode } from "../utils/AppError";
 
 // to get all user
-// export const Get
+export const GetAllUser = asyncHandler(
+    async(req:Request,res:Response,next:NextFunction):Promise<Response>=>{
+        const user = await UserModel.find()
+        if(!user){
+            new AppError({
+                message:"couldn't get all user",
+                httpCode:HttpCode.NOT_FOUND,
+            })
+        }
+        return res.status(HttpCode.OK).json({
+            message:"Successfully got all users",
+            data:user
+        })
+    }
+)
