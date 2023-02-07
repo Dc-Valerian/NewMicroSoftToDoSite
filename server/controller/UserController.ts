4ec489c54c4150bb75c21e41446b8cde92c9799e
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-07 17:10:19 +0100

    added the utils folder for error handling

---
+++ server/controller/UserController.ts
@@ -0,0 +1,5 @@
+import { Request,Response,NextFunction } from "express";
+import UserModel from "../model/UserModel";
+
+// to get all user
+// export const Get
---
+++ server/utils/AppError.ts
@@ -0,0 +1,39 @@
+export enum HttpCode{
+    OK= 200,
+    CREATED=201,
+    BAD_REQUEST=400,
+    UNAUTHORIZED= 401,
+    FORBIDDEN=403,
+    NOT_FOUND=404,
+    CONFLICT=409,
+    INTERNAL_SERVER_ERROR=500,
+    UNPROCESSABLE_IDENTITY=422,
+}
+
+interface ErrorArgs{
+    name?:string;
+    isOperational?:boolean;
+    message:string;
+    httpCode:HttpCode;
+}
+
+export class AppError extends Error{
+    public readonly name: string;
+    public readonly isOperational:boolean = true
+    public readonly httpCode:HttpCode;
+
+    constructor(args:ErrorArgs){
+        super(args.message)
+
+
+        Object.setPrototypeOf(this,new.target.prototype)
+
+        this.name = args.name || "Error"
+        this.httpCode = args.httpCode
+
+        if(args.isOperational !== undefined){
+            this.isOperational = args.isOperational
+        }
+        Error.captureStackTrace(this);
+    }
+}
