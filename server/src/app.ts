7fa8cc3eb32e64abd87fb847e4ead2ebdc47fa47
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-11 17:37:43 +0100

    updated the app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,21 @@
 import express,{Application,NextFunction,Request,Response} from "express"
 import morgan from "morgan"
 import cors from "cors"
-
+import userRouter from "../Routes/UserRouter"
+import taskRoute from "../Routes/TaskRouter"
+import { AppError,HttpCode } from "../utils/AppError"
 
 export const appConfig=(app:Application)=>{
-    app.use(morgan("dev")).use(express.json()).use(cors())
+    app.use(morgan("dev")).use(express.json()).use(cors()).use("/api",userRouter);
+
+    app.use("/api/task",taskRoute)
+
+    app.all("*",(req:Request,res:Response,next:NextFunction)=>{
+        next(
+            new AppError({
+                message:`This Route ${req.originalUrl} doesn't exist`,
+                httpCode:HttpCode.NOT_FOUND
+            })
+        )
+    })
 }
